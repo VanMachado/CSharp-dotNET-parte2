63534bb9d9af070d8542ba4172747558c6e7825e
VanMachado <vandamme.machado@gmail.com>
2022-08-26 12:43:48 -0300

    Conceitos básicos de manipulação de arquivos

---
+++ ExercicioFixacaoArquivos/ExercicioFixacaoArquivos/Program.cs
@@ -0,0 +1,19 @@
+﻿string sourcePath = @"D:\workspace\file1.txt";
+string targetpath = @"D:\workspace\file2.txt";
+
+try
+{
+    FileInfo fileInfo = new FileInfo(sourcePath);
+    fileInfo.CopyTo(targetpath);
+
+    string[] lista = File.ReadAllLines(sourcePath);
+
+    foreach(string line in lista)
+    {
+        Console.WriteLine(line);
+    }
+}
+catch (IOException e)
+{
+    Console.WriteLine("An error occured! " + e.Message);
+}
