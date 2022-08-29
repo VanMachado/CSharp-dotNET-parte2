be65ece830828225158c4bdcbecc0adc90befcfb
VanMachado <vandamme.machado@gmail.com>
2022-08-29 14:11:47 -0300

    Exercicio de fixação

---
+++ ExercicioFixacaoArquivos/ExercicioFixacaoArquivos/Entities/Products.cs
@@ -0,0 +1,25 @@
+﻿namespace ExercicioFixacaoArquivos.Entities
+{
+    internal class Products
+    {
+        public string Name { get; private set; }
+        public double Price { get; private set; }
+        public int Quantity { get; private set; }
+
+        public Products()
+        {
+        }
+
+        public Products(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;    
+        }
+
+        public double Total()
+        {
+            return Price * Quantity;
+        }        
+    }
+}
--- ExercicioFixacaoArquivos/ExercicioFixacaoArquivos/Program.cs
+++ ExercicioFixacaoArquivos/ExercicioFixacaoArquivos/Program.cs
@@ -1,17 +1,39 @@
-﻿string path = @"D:\workspace\file1.txt";
+﻿using ExercicioFixacaoArquivos.Entities;
+using System.Globalization;
+
+string sourcePath = @"D:\workspace\myfolder\Produtos1.csv";
 
 try
 {
-    Console.WriteLine("DirectorySepareteChar: " + Path.DirectorySeparatorChar);
-    Console.WriteLine(Path.PathSeparator);
-    Console.WriteLine("GetDirectoryName: " + Path.GetDirectoryName(path));
-    Console.WriteLine("GetFileName: " + Path.GetFileName(path));
-    Console.WriteLine("GetFileNameWithoutExtension: " + Path.GetFileNameWithoutExtension(path));
-    Console.WriteLine("GetExtension: " + Path.GetExtension(path));
-    Console.WriteLine("GetFullPath: " + Path.GetFullPath(path));
-    Console.WriteLine("GetTempPath: " + Path.GetTempPath());
+    string[] lines = File.ReadAllLines(sourcePath);
+    
+    string sourceFolderPath = Path.GetDirectoryName(sourcePath);
+    string targetFolderPath = sourceFolderPath + @"\out";
+    string targetFilePatth = targetFolderPath + @"\sumary.csv";
+
+    Directory.CreateDirectory(targetFolderPath);
+
+    using (StreamWriter sw = File.AppendText(targetFilePatth))
+    {
+        foreach (string line in lines)
+        {
+            string[] fields = line.Split(',');
+            string name = fields[0];
+            double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+            int quantity = int.Parse(fields[2]);
+
+            Products prod = new Products(name, price, quantity);
+
+            sw.WriteLine(prod.Name + ", " + prod.Total().
+                ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
 }
 catch (IOException e)
 {
     Console.WriteLine("An error occurred! " + e.Message);
+}
+catch(Exception e)
+{
+    Console.WriteLine(e.Message);
 }
