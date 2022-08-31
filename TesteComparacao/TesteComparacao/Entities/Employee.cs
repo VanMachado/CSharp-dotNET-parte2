dc738735375763c90b2ae7a98a8e4e2293bb90e4
VanMachado <vandamme.machado@gmail.com>
2022-08-31 18:22:20 -0300

    Usando o ICompare

---
+++ TesteComparacao/TesteComparacao/Entities/Employee.cs
@@ -0,0 +1,30 @@
+﻿namespace Entities
+{
+    internal class Employee : IComparable
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(string csvEmployee)
+        {
+            string[] csv = csvEmployee.Split(',');
+            Name = csv[0];
+            Salary = double.Parse(csv[1]);
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Salary.ToString("F2");
+        }
+
+        public int CompareTo(object? obj)
+        {
+            if (!(obj is Employee))
+            {
+                throw new ArgumentException("Objeto nao é trabalhador");
+            }
+            Employee other = (Employee)obj;
+            return Name.CompareTo(other.Name);
+        }
+    }
+}
---
+++ TesteComparacao/TesteComparacao/Program.cs
@@ -0,0 +1,41 @@
+﻿using Entities;
+
+namespace TesteComparacao
+{
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string path = @"D:\workspace\in.txt";
+            string target = @"D:\workspace\in2.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<Employee> employee = new List<Employee>();
+                    while (!sr.EndOfStream)
+                    {
+                        employee.Add(new Employee(sr.ReadLine()));
+                    }
+
+                    employee.Sort();
+
+                    foreach (Employee str in employee)
+                    {
+                        Console.WriteLine(str);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
