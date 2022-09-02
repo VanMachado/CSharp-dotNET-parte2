b67ef737f79eafbb2bf08bf936012e82b5bdeeae
VanMachado <vandamme.machado@gmail.com>
2022-09-02 15:33:13 -0300

    Utilização do Generics

---
+++ ExercicioFixacaoConjuntos/ExercicioFixacaoConjuntos/Entities/PrintService.cs
@@ -0,0 +1,32 @@
+﻿namespace ExercicioFixacaoConjuntos.Entities
+{
+    internal class PrintService
+    {
+        public List<int> Values { get; set; } = new List<int>();
+
+        public void AddValue(int value)
+        {
+            for (int i = 0; i < value; i++)
+            {
+                int valores = int.Parse(Console.ReadLine());
+                Values.Add(valores);
+            }
+        }
+
+        public int First()
+        {
+            return Values.First();
+        }
+
+        public void Print()
+        {
+            Console.Write("[ ");
+            Values.Append(0);
+            foreach (int value in Values)
+            {
+                Console.Write(value + " ");
+            }
+            Console.Write("]");
+        }
+    }
+}
---
+++ ExercicioFixacaoConjuntos/ExercicioFixacaoConjuntos/Program.cs
@@ -0,0 +1,21 @@
+﻿using ExercicioFixacaoConjuntos.Entities;
+
+try
+{
+    Console.Write("How many values to insert? ");
+    int n = int.Parse(Console.ReadLine());
+
+    PrintService printer = new PrintService();
+
+    printer.AddValue(n);
+
+    Console.WriteLine();
+    printer.Print();
+    Console.WriteLine();
+    Console.Write("First: ");
+    Console.WriteLine(printer.First());
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
