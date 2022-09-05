bcb061e06998f4b7da3e194046430f9a7e04d425
VanMachado <vandamme.machado@gmail.com>
2022-09-05 18:22:00 -0300

    Utilizacao do Equals e GetHashCode para customizar comparacoes

---
+++ ExercicioFixacaoConjuntos/ExercicioFixacaoConjuntos/Entities/Client.cs
@@ -0,0 +1,33 @@
+﻿namespace Entities
+{
+    internal class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public Client(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+
+        //Aqui eu defino o criterio para entender se meu objeto eh ou nao igual a outr, no caso
+        //o parametro escolhido foi o "email"
+        public override bool Equals(object? obj)
+        {
+            if(!(obj is Client))
+            {
+                return false;
+            }
+
+            Client other = (Client)obj;    
+            
+            return Email.Equals(other.Email);
+        }
+
+        public override int GetHashCode()
+        {           
+            return Email.GetHashCode();
+        }
+    }
+}
--- ExercicioFixacaoConjuntos/ExercicioFixacaoConjuntos/Program.cs
+++ ExercicioFixacaoConjuntos/ExercicioFixacaoConjuntos/Program.cs
@@ -1,22 +1,6 @@
 ﻿using Entities;
-using Services;
 
-Console.Write("Enter N: ");
-int n = int.Parse(Console.ReadLine());
+Client a = new Client("Maria", "maria@gmail.com");
+Client b = new Client("Alex", "alex@gmail.com");
 
-List<Products> lista = new List<Products>();
-
-for (int i = 0; i < n; i++)
-{
-    string[] fields = Console.ReadLine().Split(',');
-    string name = fields[0];
-    double value = double.Parse(fields[1]);
-    
-    lista.Add(new Products(name, value));
-}
-
-Console.WriteLine("MAX:");
-
-CalculationService service = new CalculationService();
-
-Console.WriteLine(service.Max(lista));
+Console.WriteLine(a.GetHashCode());
