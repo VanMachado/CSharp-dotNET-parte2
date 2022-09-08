f4c3cea3a953b80d3d1177ab641fadd1b45eaa7a
VanMachado <vandamme.machado@gmail.com>
2022-09-08 18:01:29 -0300

    Utilizacao do SortedSet com metodos genericos

--- ExercicioFixacaoConjuntos/ExercicioFixacaoConjuntos/Program.cs
+++ ExercicioFixacaoConjuntos/ExercicioFixacaoConjuntos/Program.cs
@@ -1,6 +1,50 @@
-﻿using Entities;
+﻿namespace Conjuntos
+{
+    class Program
+    {
+        static public void Main(string[] args)
+        {
+            SortedSet<int> a = new SortedSet<int>() { 0, 2, 4, 5, 6, 8, 10 };
+            SortedSet<int> b = new SortedSet<int>() { 5, 6, 7, 8, 9, 10 };
 
-Client a = new Client("Maria", "maria@gmail.com");
-Client b = new Client("Alex", "alex@gmail.com");
+            //PrintCollector<int>(a);
 
-Console.WriteLine(a.GetHashCode());
+            //Uniao
+            SortedSet<int> c = new SortedSet<int> (a);
+            Console.Write("Uniao: [ ");
+            c.UnionWith(b);
+            PrintCollector<int>(c);
+            Console.Write("]");
+            Console.WriteLine();
+
+            Console.WriteLine("-----------------------------------");
+            
+            //Intersecao            
+            SortedSet<int> d = new SortedSet<int>(a);
+            d.IntersectWith(b);
+            Console.Write("Intersecao: [ ");
+            PrintCollector(d);
+            Console.Write("]");
+            Console.WriteLine();
+
+            Console.WriteLine("-----------------------------------");
+
+            //Diferenca
+            SortedSet<int> e = new SortedSet<int> (a);
+            e.ExceptWith(b);
+            Console.Write("Difrenca: [ ");
+            PrintCollector(e);
+            Console.Write("]");
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        static void PrintCollector<T>(IEnumerable<T> collection) where T : IComparable
+        {
+            foreach (T item in collection)
+            {
+                Console.Write(item + " ");
+            }            
+        }
+    }
+}
