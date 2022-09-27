df7a03f95dd617b2fa5cd41e7a002d49f4231b8a
VanMachado <vandamme.machado@gmail.com>
2022-09-27 12:53:08 -0300

    Utilizando extension methods com datas

---
+++ ExtensionMethods/ExtensionMethods/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,22 @@
+﻿//Quando se faz um "extension method", por boa pratica, coloque-o no mesmo namespace da classe
+//que sera extendida
+
+namespace System
+{
+    static class DateTimeExtensions
+    {
+        public static string ElapsedTime(this DateTime thisObj)
+        {
+            TimeSpan durantion = DateTime.Now.Subtract(thisObj);
+
+            if(durantion.TotalHours < 24)
+            {
+                return durantion.TotalHours.
+                    ToString("F1") + " Hours";
+            }
+
+            return durantion.TotalDays.
+                ToString("F1") + " Days";
+        }
+    }
+}
---
+++ ExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,3 @@
+﻿DateTime dt = new DateTime(2022, 9, 27, 10, 10, 45);
+
+Console.WriteLine(dt.ElapsedTime());
