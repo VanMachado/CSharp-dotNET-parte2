af01af74aa177e6c00a7d91f3b8b31cb8d05998d
VanMachado <vandamme.machado@gmail.com>
2022-10-03 17:19:07 -0300

    Usando multicasting delegates

--- ExercicioFixacaoExpressoesLambda/ExercicioFixacaoExpressoesLambda/Program.cs
+++ ExercicioFixacaoExpressoesLambda/ExercicioFixacaoExpressoesLambda/Program.cs
@@ -1,20 +1,20 @@
-﻿using Entities;
+﻿using Service;
 
-List<Products> products = new List<Products>();
-
-products.Add(new Products(900.00, "TV"));
-products.Add(new Products(1200.00, "Notebook"));
-products.Add(new Products(450.00, "Tablet"));
-
-
-//Pode ser feito assim, ou simplesmente declarando a funcao lambada no argumento
-//Comparison<Products> comp = (p1, p2) => p1.Name.ToUpper().
-//    CompareTo(p2.Name.ToUpper());
-
-products.Sort((p1, p2) => p1.Name.ToUpper().
-    CompareTo(p2.Name.ToUpper()));
-
-foreach (Products p in products)
+namespace ExercicioFixacaoExpressoesLambda
 {
-    Console.WriteLine(p);
+    delegate void BinaryNumericOperation(double n1, double n2);
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.ShowSum;
+            op += CalculationService.ShowMax;
+
+            op(a, b);            
+        }
+    }
 }
