9f3cea0265eb0f3443145cfc0e45c0c9199afa6d
VanMachado <vandamme.machado@gmail.com>
2022-10-03 17:39:25 -0300

    Utilizando o predicate

--- ExercicioFixacaoExpressoesLambda/ExercicioFixacaoExpressoesLambda/Program.cs
+++ ExercicioFixacaoExpressoesLambda/ExercicioFixacaoExpressoesLambda/Program.cs
@@ -1,20 +1,32 @@
-﻿using Service;
+﻿using Entities;
 
 namespace ExercicioFixacaoExpressoesLambda
-{
-    delegate void BinaryNumericOperation(double n1, double n2);
-
+{   
     class Program
     {
         public static void Main(string[] args)
         {
-            double a = 10;
-            double b = 12;
+            List<Product> list = new List<Product>();
 
-            BinaryNumericOperation op = CalculationService.ShowSum;
-            op += CalculationService.ShowMax;
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
 
-            op(a, b);            
+            list.RemoveAll(ProductTest);
+
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+
+        
+        //O predicate é um tipo de delegate que recebe um objeto do Tipo T como argunento
+        //e retorna um boolenao como resposta
+        public static bool ProductTest(Product p)
+        {
+            return p.Price <= 100.00;
         }
     }
 }
