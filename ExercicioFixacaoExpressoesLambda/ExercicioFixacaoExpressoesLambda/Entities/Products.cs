3022460bd82ce37611457d46e0e8b156f58a32cd
VanMachado <vandamme.machado@gmail.com>
2022-10-03 16:12:22 -0300

    Utilizando o CompareTo sem programação funcional

---
+++ ExercicioFixacaoExpressoesLambda/ExercicioFixacaoExpressoesLambda/Entities/Products.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+namespace Entities
+{
+    internal class Products : IComparable<Products>
+    {
+        public double Price { get; set; }
+        public string Name { get; set; }
+
+        public Products(double price, string name)
+        {
+            Price = price;
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Price.
+                ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public int CompareTo(Products other)
+        {                        
+            return Name.ToUpper().CompareTo(other.Name.ToUpper());
+        }
+    }
+}
---
+++ ExercicioFixacaoExpressoesLambda/ExercicioFixacaoExpressoesLambda/Program.cs
@@ -0,0 +1,14 @@
+﻿using Entities;
+
+List<Products> products = new List<Products>();
+
+products.Add(new Products(900.00, "TV"));
+products.Add(new Products(1200.00, "Notebook"));
+products.Add(new Products(450.00, "Tablet"));
+
+products.Sort();
+
+foreach(Products p in products)
+{
+    Console.WriteLine(p);
+}
